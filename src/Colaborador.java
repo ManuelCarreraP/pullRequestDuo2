445263fc56cb8f84aeae93aa9629c0df24b72b8e
Borja De Saa <bdesaavazquez@danielcastelao.org>
2025-01-29 14:34:32 +0100

    Colaborador 2

--- src/Colaborador.java
+++ src/Colaborador.java
@@ -2,5 +2,6 @@ public class Colaborador {
     public static void main(String[] args) {
         System.out.println("Me llamo Borja y soy el colaborador de Manuel");
         System.out.println("Commit 1");
+        System.out.println("Commit 2");
     }
 }
