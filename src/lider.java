721cf10dff1ec3c06d66930f5602acab199a782f
Manuel Carrera Pazó <mcarrerapazo@danielcastelao.org>
2025-01-29 14:33:42 +0100

    Commit 1

---
+++ src/lider.java
@@ -0,0 +1,6 @@
+public class lider {
+    public static void main(String[] args) {
+        System.out.println("Soy el lider del proyecto (Manuel Carrera) y mi colaborador es Borja");
+        System.out.println("Commit Lider 1");
+    }
+}
